edbf0baa1db23cb5222e4f13e551589581e890d0
wanglei <evseiy@wzhangxin.cn>
2018-11-06 17:47:45 +0800

    替换 更新

---
+++ Bill_Server/Bill_Server/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bill_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SocketServer socketServer = new SocketServer("127.0.0.1");
+            socketServer.Listenin(1000);
+            Console.ReadLine();
+        }
+    }
+}
