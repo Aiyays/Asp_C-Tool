8771de7b0050017129ad46e6c743cb2d6c3c476a
wanglei <evseiy@wzhangxin.cn>
2018-11-09 17:56:29 +0800

    网页快照

---
+++ HTML_Snapshot/TestSnap/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestSnap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SnapLibrary.Snapshot snap = new SnapLibrary.Snapshot();
+            Bitmap bmp = snap.TakeSnapshot(this.webBrowser1.ActiveXInstance, new Rectangle(0, 0, this.webBrowser1.Width, this.webBrowser1.Height));
+            if (bmp != null)
+            {
+                bmp.Save("1.bmp");
+                bmp.Dispose();
+            }
+        }
+    }
+}
